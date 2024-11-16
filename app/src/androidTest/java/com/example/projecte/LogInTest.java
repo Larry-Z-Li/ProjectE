6e1abfed11e0414d8d5324d8c54a304cb9a62975
Jimena Arce <arcecant@usc.edu>
2024-11-15 20:04:28 -0800

    login test2

--- app/src/androidTest/java/com/example/projecte/LogInTest.java
+++ app/src/androidTest/java/com/example/projecte/LogInTest.java
@@ -1,7 +1,11 @@
 package com.example.projecte;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import org.junit.Rule;
@@ -21,5 +25,10 @@ public class LogInTest {
     public void testCorrectLogin()
     {
         onView(withId(R.id.username)).perform(typeText("userTest"));
+        onView(withId(R.id.username)).perform(typeText("userPassword"));
+        onView(withId(R.id.loginButton)).perform(click());
+
+        onView(withText("Courses")).check(matches(isDisplayed()));
+        System.out.println("Login Successful");
     }
 }
