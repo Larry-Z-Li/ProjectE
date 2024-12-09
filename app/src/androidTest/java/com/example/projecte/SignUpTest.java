f1e68380b20b18c218564cd0207bc2cdfb6a15dc
Jimena Arce <arcecant@usc.edu>
2024-12-08 22:45:55 -0800

    edits 4

--- app/src/androidTest/java/com/example/projecte/SignUpTest.java
+++ app/src/androidTest/java/com/example/projecte/SignUpTest.java
@@ -9,6 +9,7 @@ import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.hasErrorText;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
